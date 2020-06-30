95eb37ba2a1558d55adfb16c8b8795fbde1067ff
Becke <bilguun.dama@yahoo.com>
2020-06-30 18:41:35 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчил санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // <div class="player-score" id="score-0">43</div>
 // window.document.querySelector("#score-0").textContent = dice;
@@ -16,12 +16,16 @@ var dice = Math.floor(Math.random() * 6) + 1;
 // document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
 
 // Тоглоом эхлэхэд бэлтгэе
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-1").textContent = "0";
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
 
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".dice").style.display = "none";
-
-console.log("Шоо :" + dice);
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
